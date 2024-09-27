94bf21d93132790b48375e95a2791f8840adb97e
天魂 <365125264@qq.com>
2024-09-27 15:59:33 +0800

    feat: add shadcn UI component library and r elated configurations

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
+                <Button>Click me</Button>
                 <w3m-button />
                 <Image
                     className={styles.logo}
--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig, presetUno, presetAttributify } from 'unocss';
+import { presetShadcn } from 'unocss-preset-shadcn';
+import presetAnimations from 'unocss-preset-animations';
 
 export default defineConfig({
     presets: [
         presetUno(),
         presetAttributify(),
+        presetShadcn({
+            color: 'zinc',
+        }),
+        presetAnimations(),
     ],
 });
